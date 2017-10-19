01de47508052b33683bab0b580c1d6c03a87390a
limembo <lmembis@gmail.com>
2017-10-19 06:06:20 -0400

    updated app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function onReady(){
   // Call renderTheUI() each time state changes
   newToDoText.value = '';
   renderTheUI(toDos);
-
+}
   // Add the event listener; we want it to listen for a "submission"
     addToDoForm.addEventListener('submit', event => {
       event.preventDefault();
@@ -27,12 +27,12 @@ function onReady(){
     toDos.forEach(function(toDo){
 // Create UI for each to-do, assign toDo's title to li and update dom with li and checkbox
       const newLi = document.createElement('li');
-      const checkbox = document.createElement('input')
+      const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 // Add toDo's title text to newLi
       newLi.textContent = toDo.title;
 //Update the DOM
-      todoList.appendChild(newLi);
+      toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
     });
     }
